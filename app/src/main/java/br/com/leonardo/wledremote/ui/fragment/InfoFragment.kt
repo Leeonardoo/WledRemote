2034c7a10e07d1726a599f5c13a712b4ab5608b5
Leonardo de Oliveira <leonardo.oliveira@velow.com.br>
2020-07-10 22:24:15 -0300

    Fixed info list update Added diffcallback to InfoAdapter.kt for the animations

--- app/src/main/java/br/com/leonardo/wledremote/ui/fragment/InfoFragment.kt
+++ app/src/main/java/br/com/leonardo/wledremote/ui/fragment/InfoFragment.kt
@@ -20,6 +20,7 @@ import br.com.leonardo.wledremote.model.ui.InfoItem
 import br.com.leonardo.wledremote.rest.api.ResultWrapper
 import br.com.leonardo.wledremote.ui.activity.viewmodel.MainViewModel
 import br.com.leonardo.wledremote.ui.fragment.viewmodel.DashboardViewModel
+import br.com.leonardo.wledremote.util.convertMillisToDisplay
 
 
 class InfoFragment : Fragment() {
@@ -27,6 +28,7 @@ class InfoFragment : Fragment() {
     private lateinit var binding: FragmentInfoBinding
     private val viewModel: DashboardViewModel by viewModels()
     private val mainViewModel: MainViewModel by activityViewModels()
+    private lateinit var adapter: InfoAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -39,6 +41,25 @@ class InfoFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+        adapter = InfoAdapter()
+
+        binding.infoItemsRecyclerView.adapter = adapter
+        binding.infoItemsRecyclerView.layoutManager =
+            StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+
+        val spacing = resources.getDimensionPixelSize(R.dimen.info_item_spacing) / 2
+
+        binding.infoItemsRecyclerView.setPadding(spacing, spacing, spacing, spacing)
+        binding.infoItemsRecyclerView.clipToPadding = false
+        binding.infoItemsRecyclerView.clipChildren = false
+        binding.infoItemsRecyclerView.addItemDecoration(object : ItemDecoration() {
+            override fun getItemOffsets(
+                outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State
+            ) {
+                outRect.set(spacing, spacing, spacing, spacing)
+//                super.getItemOffsets(outRect, view, parent, state)
+            }
+        })
 
         setListeners()
         setObservers()
@@ -59,33 +80,17 @@ class InfoFragment : Fragment() {
     }
 
     private fun displayInfo(info: Info) {
-        val testInfos = listOf(
+        val infos = listOf(
             InfoItem(
                 "Power Usage",
                 R.drawable.power_outline_primary_24dp,
                 "${info.leds?.currentPowerUsage}mA"
             ),
-            InfoItem("Uptime", R.drawable.timeline_primary_24dp, "${info.uptime} since last reset")
+            InfoItem("Uptime", R.drawable.timeline_primary_24dp, "${info.uptime?.let {
+                convertMillisToDisplay(it)
+            }} since last reset")
         )
 
-        val adapter = InfoAdapter(testInfos)
-        binding.infoItemsRecyclerView.adapter = adapter
-
-        binding.infoItemsRecyclerView.layoutManager =
-            StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
-
-        val spacing = resources.getDimensionPixelSize(R.dimen.info_item_spacing) / 2
-
-        binding.infoItemsRecyclerView.setPadding(spacing, spacing, spacing, spacing)
-        binding.infoItemsRecyclerView.clipToPadding = false
-        binding.infoItemsRecyclerView.clipChildren = false
-        binding.infoItemsRecyclerView.addItemDecoration(object : ItemDecoration() {
-            override fun getItemOffsets(
-                outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State
-            ) {
-                outRect.set(spacing, spacing, spacing, spacing)
-//                super.getItemOffsets(outRect, view, parent, state)
-            }
-        })
+        adapter.updateList(infos)
     }
 }
